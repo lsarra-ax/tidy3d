796e97a28e76329305e9e37300a895f7dd1786f9
daquinteroflex <dario@flexcompute.com>
2024-11-15 15:26:23 +0100

    Let's see

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 from nox_poetry import session
 
-@session(python=["3.10", "3.11", "3.12"])
+@session(python=["3.9", "3.10", "3.11", "3.12"])
 def tests(session):
     session.install('.')
     session.run('pytest')
