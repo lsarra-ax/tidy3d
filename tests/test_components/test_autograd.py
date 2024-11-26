fixed slab bounds numerical tests in specific situations

tests/test_components/test_autograd.py
@@ -84,7 +84,7 @@ DA_SHAPE_X = 1 if IS_3D else 1
 DA_SHAPE = (DA_SHAPE_X, 1_000, 1_000) if TEST_CUSTOM_MEDIUM_SPEED else (DA_SHAPE_X, 12, 12)
 
 # number of vertices in the polyslab
-NUM_VERTICES = 100_000 if TEST_POLYSLAB_SPEED else 4
+NUM_VERTICES = 100_000 if TEST_POLYSLAB_SPEED else 25
 
 PNT_DIPOLE = td.PointDipole(
     center=(0, 0, -LZ / 2 + WVL),
@@ -345,16 +345,17 @@ def make_structures(params: anp.ndarray) -> dict[str, td.Structure]:
     matrix = np.random.random((N_PARAMS,)) - 0.5
     params_01 = 0.5 * (anp.tanh(matrix @ params / 3) + 1)
 
-    free_param = "vertices" if POLYSLAB_AXIS in [0, 2] else "slab_bounds"
+    free_param = "vertices" if POLYSLAB_AXIS == 0 else "slab_bounds"
 
     if free_param == "vertices":
         radii = 0.5 + 0.5 * params_01
         slab_bounds = (-0.5, 0.5)
     elif free_param == "slab_bounds":
         radii = 1.0
-        slab_bounds = (-0.5 * params_01, 0.5 * params_01)  # 5%, 0.01
-        # slab_bounds = (-0.5 * params_01, 0.5) # -1x off
-        # slab_bounds = (-0.5, 0.5 * params_01) # 5%, 0.95
+        shift = 0.1 * params_01
+        slab_bounds = (-0.5 + shift, 0.5 + shift)
+        # slab_bounds = (-0.5 + shift, 0.5)
+        # slab_bounds = (-0.5, 0.5 + shift)
 
     phis = 2 * anp.pi * anp.linspace(0, 1, NUM_VERTICES + 1)[:NUM_VERTICES]
     xs = radii * anp.cos(phis)
@@ -581,7 +582,7 @@ if TEST_POLYSLAB_SPEED:
     args = [("polyslab", "mode")]
 
 
-args = [("polyslab", "mode")]
+# args = [("polyslab", "mode")]
 
 
 def get_functions(structure_key: str, monitor_key: str) -> typing.Callable:
@@ -667,7 +668,7 @@ def test_autograd_numerical(structure_key, monitor_key):
         if PLOT_SIM:
             plot_sim(sim, plot_eps=True)
 
-        data = web.run(sim, task_name="autograd_test_numerical", verbose=False)
+        data = web.run(sim, task_name="autograd_test_numerical", verbose=False, local_gradient=True)
         value = postprocess(data)
         return value
 
@@ -676,7 +677,7 @@ def test_autograd_numerical(structure_key, monitor_key):
     assert anp.all(grad != 0.0), "some gradients are 0"
 
     # numerical gradients
-    delta = 1e-2
+    delta = 1e-1
     sims_numerical = {}
 
     params_num = np.zeros((N_PARAMS, N_PARAMS))

tidy3d/components/geometry/polyslab.py
@@ -1421,10 +1421,10 @@ class PolySlab(base.Planar):
         ax_min, (r1_min, r2_min) = self.pop_axis(rmin, axis=self.axis)
         ax_max, (r1_max, r2_max) = self.pop_axis(rmax, axis=self.axis)
 
-        num_x = 1 if r1_min == r1_max else _NUM_PTS_DIM_SLAB_BOUNDS
-        num_y = 1 if r1_min == r2_max else _NUM_PTS_DIM_SLAB_BOUNDS
+        num_1 = 1 if r1_min == r1_max else _NUM_PTS_DIM_SLAB_BOUNDS
+        num_2 = 1 if r2_min == r2_max else _NUM_PTS_DIM_SLAB_BOUNDS
 
-        num_cells = num_x * num_y
+        num_cells = num_1 * num_2
         ones = np.ones(num_cells)
         zeros = np.zeros(num_cells)
 
@@ -1435,8 +1435,8 @@ class PolySlab(base.Planar):
             return np.stack(coords, axis=-1)
 
         # get center points and areas
-        r1_centers = np.linspace(r1_min, r1_max, 2 * num_x + 1)[1::2]
-        r2_centers = np.linspace(r2_min, r2_max, 2 * num_y + 1)[1::2]
+        r1_centers = np.linspace(r1_min, r1_max, 2 * num_1 + 1)[1::2]
+        r2_centers = np.linspace(r2_min, r2_max, 2 * num_2 + 1)[1::2]
         planar_centers = meshgrid_flatten_stack(r1_centers, r2_centers)
 
         area = 1.0
@@ -1444,7 +1444,7 @@ class PolySlab(base.Planar):
             if rmin != rmax:
                 area *= rmax - rmin
 
-        areas = area * np.ones(num_cells)
+        areas = area * np.ones(num_cells) / num_cells
 
         def get_grad(min_max_index: int) -> float:
             """Compute gradient for either min or max dimension."""