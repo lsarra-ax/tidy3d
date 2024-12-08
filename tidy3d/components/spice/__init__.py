9b38243cf971482cca1224d7cc69dafe8d4acc41
daquinteroflex <dario@flexcompute.com>
2024-12-08 19:23:35 +0100

    I don't get what happened

--- tidy3d/components/spice/__init__.py
+++ tidy3d/components/spice/__init__.py
@@ -6,3 +6,17 @@ a SPICE model.
 All of the types in this directory should follow the standards defined in this documentation:
 https://ngspice.sourceforge.io/docs/ngspice-manual.pdf
 """
+
+from tidy3d.components.spice.analysis.dc import OperatingPointDC, TransferFunctionDC
+from tidy3d.components.spice.components.capacitors import Capacitor
+from tidy3d.components.spice.sources.dc import MultiStaticTransferSourceDC, StaticTransferSourceDC
+from tidy3d.components.spice.types import ElectricalAnalysisTypes
+
+__all__ = [
+    "Capacitor",
+    "ElectricalAnalysisTypes",
+    "OperatingPointDC",
+    "MultiStaticTransferSourceDC",
+    "StaticTransferSourceDC",
+    "TransferFunctionDC",
+]
